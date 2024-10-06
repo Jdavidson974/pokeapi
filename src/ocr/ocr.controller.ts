2efc726cb46fc4807e9843fd9354f920dab3caae
JD <jd9743101@gmail.com>
2024-10-06 19:55:08 +0200

    reactivation de ocr

--- src/ocr/ocr.controller.ts
+++ src/ocr/ocr.controller.ts
@@ -1,18 +1,26 @@
 import { Controller, Post, UploadedFile, UseInterceptors } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { OcrService } from './ocr.service';
-
+import * as fs from 'fs';
 @Controller('ocr')
 export class OcrController {
-  constructor(private readonly ocrService: OcrService) { }
+  constructor(private readonly ocrService: OcrService,) { }
 
-  // @Post('upload')
-  // @UseInterceptors(FileInterceptor('file', { dest: 'src/uploads' })) // Assurez-vous que 'file' correspond à la clé du champ dans Postman
-  // async uploadFile(@UploadedFile() file: Express.Multer.File) {
-  //   if (!file) {
-  //     throw new Error('Aucun fichier téléchargé');
-  //   }
-  //   const text = await this.ocrService.recognizeText(file.path); // Utilise file.path
-  //   return { text };
-  // }
+  @Post('upload')
+  @UseInterceptors(FileInterceptor('file', { dest: '/uploads' })) // Assurez-vous que 'file' correspond à la clé du champ dans Postman
+  async uploadFile(@UploadedFile() file: Express.Multer.File) {
+    if (!file) {
+      throw new Error('Aucun fichier téléchargé');
+    }
+    const text = await this.ocrService.recognizeText(file.path).then(
+      data => {
+        fs.unlink(file.path, (err) => {
+          console.log("Fichier Supprimer");
+        })
+        return { data };
+      }
+    ); // Utilise file.path
+
+
+  }
 }
